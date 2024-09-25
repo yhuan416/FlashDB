32fc7ff7969a3fc7ef95cb267b066b5fd76d906c
yangzelin <yhuan416@foxmail.com>
2024-09-25 16:10:39 +0800

    feat: update

--- lkm/README.md
+++ lkm/README.md
@@ -1,4 +1,8 @@
 
+ARCH: architecture
+KERNEL: kernel path
+CROSS_COMPILE: toolchain
+
 ``` sh
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- KERNEL=/home/yhuan/workspace/stm32mp13/linux/linux-5.15.24
 ```
---
+++ lkm/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- KERNEL=/home/yhuan/workspace/stm32mp13/linux/linux-5.15.24 clean
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- KERNEL=/home/yhuan/workspace/stm32mp13/linux/linux-5.15.24
